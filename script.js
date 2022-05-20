8f3af4a6b6c0e17cb46fbf47cb5da2c4fa6eb796
Warsame <osman18@my.yorku.ca>
2022-05-20 18:10:10 -0400

    partially done

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function computerPlay() {
-  let computerGen = Math.floor(Math.rand() * 3);
+  let computerGen = Math.floor(Math.random() * 3);
 
   switch (computerGen) {
     case 0:
@@ -15,6 +15,49 @@ function computerPlay() {
   return "error";
 }
 
-function playRound(playSelection, computerSelection) {}
+function playRound(playerSelection, computerSelection) {
+  let player, comp;
+  playerSelection = playerSelection.toLowerCase();
+  computerSelection = computerSelection.toLowerCase();
+  if (playerSelection == computerSelection) {
+    return "You choose the same sign! Draw!";
+  } else if (playerSelection == "rock" && computerSelection == "paper") {
+    cpuScore(comp);
+    return "You Lose! Paper beats Rock";
+  } else if (playerSelection == "paper" && computerSelection == "scissors") {
+    return "You Lose! Scissors beats Paper";
+  } else if (playerSelection == "scissors" && computerSelection == "rock") {
+    return "You Lose! Rock beats Scissors";
+  } else if (playerSelection == "paper" && computerSelection == "rock") {
+    return "You Win! Paper beats Rock";
+  } else if (playerSelection == "rock" && computerSelection == "scissors") {
+    return "You Win! Rock beats Scissors";
+  } else if (playerSelection == "scissors" && computerSelection == "paper") {
+    return "You Win! Scissors beats Paper";
+  }
+}
 
-let gameResult = document.querySelector("#game").value;
+function game() {
+  let player, comp;
+  for (let i = 0; i < 5; i++) {
+    const playerSelection = prompt("Enter your sign!");
+    const computerSelection = computerPlay();
+    console.log(playRound(playerSelection, computerSelection));
+  }
+  if (comp > player) {
+    return "You lost to the computer!!";
+  } else if (comp < player) {
+    return "You won against the computer!!";
+  }
+  return "its a tie";
+}
+
+function cpuScore(number) {
+  return number++;
+}
+
+function playerScore(number) {
+  return number++;
+}
+
+game();
