139d08a815644154da2ab4f3e6de0e4157f9ad7e
Warsame <osman18@my.yorku.ca>
2022-05-24 00:46:03 -0400

    changes

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+//id's needed..
+const btnRock = document.querySelector("#rock");
+const btnPaper = document.querySelector("#paper");
+const btnScissors = document.querySelector("#scissors");
+const btnPlayRound = document.querySelector("#playRound");
+let playerSign = null;
+
+//generate the computers sign
 function computerPlay() {
   let computerGen = Math.floor(Math.random() * 3);
 
@@ -14,50 +22,46 @@ function computerPlay() {
   }
   return "error";
 }
-
+//play a round
 function playRound(playerSelection, computerSelection) {
   let player, comp;
   playerSelection = playerSelection.toLowerCase();
   computerSelection = computerSelection.toLowerCase();
+
   if (playerSelection == computerSelection) {
-    return "You choose the same sign! Draw!";
+    document.getElementById("results").innerHTML =
+      "You choose the same sign! Draw!";
   } else if (playerSelection == "rock" && computerSelection == "paper") {
-    cpuScore(comp);
-    return "You Lose! Paper beats Rock";
+    document.getElementById("results").innerHTML = "You Lose! Paper beats Rock";
   } else if (playerSelection == "paper" && computerSelection == "scissors") {
-    return "You Lose! Scissors beats Paper";
+    document.getElementById("results").innerHTML =
+      "You Lose! Scissors beats Paper";
   } else if (playerSelection == "scissors" && computerSelection == "rock") {
-    return "You Lose! Rock beats Scissors";
+    document.getElementById("results").innerHTML =
+      "You Lose! Rock beats Scissors";
   } else if (playerSelection == "paper" && computerSelection == "rock") {
-    return "You Win! Paper beats Rock";
+    document.getElementById("results").innerHTML = "You Win! Paper beats Rock";
   } else if (playerSelection == "rock" && computerSelection == "scissors") {
-    return "You Win! Rock beats Scissors";
+    document.getElementById("results").innerHTML =
+      "You Win! Rock beats Scissors";
   } else if (playerSelection == "scissors" && computerSelection == "paper") {
-    return "You Win! Scissors beats Paper";
+    document.getElementById("results").innerHTML =
+      "You Win! Scissors beats Paper";
   }
 }
 
-function game() {
-  let player, comp;
-  for (let i = 0; i < 5; i++) {
-    const playerSelection = prompt("Enter your sign!");
-    const computerSelection = computerPlay();
-    console.log(playRound(playerSelection, computerSelection));
-  }
-  if (comp > player) {
-    return "You lost to the computer!!";
-  } else if (comp < player) {
-    return "You won against the computer!!";
-  }
-  return "its a tie";
+//functions invoked when buttons clicked
+function buttonPlay(e) {
+  playerSign = e.target.value;
+  return (document.getElementById(
+    "pSelect"
+  ).innerHTML = `you picked ${e.target.value}`);
 }
 
-function cpuScore(number) {
-  return number++;
-}
-
-function playerScore(number) {
-  return number++;
-}
-
-game();
+//add eventListeners
+btnRock.addEventListener("click", (e) => buttonPlay(e));
+btnPaper.addEventListener("click", (e) => buttonPlay(e));
+btnScissors.addEventListener("click", (e) => buttonPlay(e));
+btnPlayRound.addEventListener("click", () =>
+  playRound(playerSign, computerPlay())
+);
